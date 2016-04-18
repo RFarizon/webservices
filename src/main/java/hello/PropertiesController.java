77f05f5eb9893488fd1438aca89ce53cdec4db4b
Ron Farizon <farizon.r@husky.neu.edu>
2016-04-18 14:30:56 -0400

    propcontroller

---
+++ src/main/java/hello/PropertiesController.java
@@ -0,0 +1,21 @@
+package main.java.hello;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import main.java.classes_for_db.DemoGeo;
+import main.java.classes_for_db.Property;
+import main.java.database.DBReader;
+
+
+@RestController
+public class PropertiesController {
+    @RequestMapping("/properties")
+    public List<Property> prop(@RequestParam String address,@RequestParam int zip) {
+    	DBReader reader = new DBReader();
+        return reader.selectProperty(address, zip);
+    }
+}
