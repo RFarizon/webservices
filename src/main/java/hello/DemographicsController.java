05078b81d6a068469badd26847bf1c17d52dcbde
Brandon <brandon.bogan93@gmail.com>
2016-04-18 13:34:26 -0400

    Fixed conflict

--- src/main/java/hello/DemographicsController.java
+++ src/main/java/hello/DemographicsController.java
@@ -1,24 +1,13 @@
 package main.java.hello;
 
-
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import classes_for_db.DemoGeo;
-import database.DBReader;
-=======
-import main.java.classes_for_db.DemoGeo;
-import main.java.database.DBReader;
->>>>>>> brandon
-=======
 import main.java.classes_for_db.DemoGeo;
 import main.java.database.DBReader;
 
->>>>>>> brandon
 
 @RestController
 public class DemographicsController {
