e5c36332ca9677e4c82289d8bab65e2428f03b3e
Brandon <brandon.bogan93@gmail.com>
2016-04-19 12:36:49 -0400

    Successfully fixed Property Controller

--- src/main/java/hello/PropertiesController.java
+++ src/main/java/hello/PropertiesController.java
@@ -13,18 +13,14 @@ import main.java.database.DBReader;
 
 @RestController
 public class PropertiesController {
-<<<<<<< HEAD
   @RequestMapping("/properties")
-  public List<Property> prop(@RequestParam String address, @RequestParam int zip) {
+  public List<Property> prop(@RequestParam(value = "address") String address,
+      @RequestParam(value = "zip") int zip) {
+    // System.out.println("THESE ARE THE PARAMS: " + address + " AND " + zip);
     DBReader reader = new DBReader();
-    return reader.selectProperty(address, zip);
+    List<Property> resultList = reader.selectProperty(address, zip);
+    System.out
+        .println("Query Complete. Number of Results Returned From DBReader: " + resultList.size());
+    return resultList;
   }
 }
-=======
-    @RequestMapping("/properties")
-    public List<Property> prop(@RequestParam String address, @RequestParam int zip) {
-    	DBReader reader = new DBReader();
-        return reader.selectProperty(address, zip);
-    }
-}
->>>>>>> master
